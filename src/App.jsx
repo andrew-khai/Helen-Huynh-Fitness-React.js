551ef96c4509e43fac6f408397ca47dbeb9dcd86
Andrew <1andrew.khai@gmail.com>
2023-11-10 13:31:15 -0800

    finished footer section and added header link animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./components/Homepage";
 import { Switch, Route } from "react-router-dom";
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <Header />
       <HomePage />
+      <Footer />
     </>
   )
 }
