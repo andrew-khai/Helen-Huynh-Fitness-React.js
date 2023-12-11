05201672dcccd2b12179e3bc7bc3e53a2be228e2
Andrew <1andrew.khai@gmail.com>
2023-12-11 13:15:13 -0800

    added training section

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./Homepage.css"
 import { FaStar } from "react-icons/fa";
 
 const HomePage = () => {
+  const [dropdown1, setDropdown1] = useState(true);
+  const [dropdown2, setDropdown2] = useState(false);
+  const [dropdown3, setDropdown3] = useState(false);
+
+  const toggleDropDown1 = () => {
+    setDropdown1(!dropdown1)
+  }
+  const toggleDropDown2 = () => {
+    setDropdown2(!dropdown2)
+  }
+  const toggleDropDown3 = () => {
+    setDropdown3(!dropdown3)
+  }
+
   return (
     <main id="main-content-container">
       <section id="about-me-container">
@@ -73,21 +88,23 @@ const HomePage = () => {
       <section id="sessions-section-container">
         <div className="sessions-container">
           <div className="sessions-info-container">
-            <div>Available Coaching Sessions</div>
+            <div className="sessions-section-title">Available Coaching Sessions</div>
             <div className="sessions-lines"></div>
             <div className="sessions-info">
               <div className="training-and-rates">
                 <div className="training-type">ONE-ON-ONE IN-PERSON TRAINING</div>
                 <div className="pricing">
                   <div>$80/hr</div>
-                  <div><i class="fa-solid fa-chevron-down"></i></div>
+                  <div onClick={toggleDropDown1} className="session-dropdown"><i className={`fa-solid fa-chevron-${dropdown1 ? 'down' : 'up'}`}></i></div>
                 </div>
               </div>
-              <div>
-                Go over goals, and current fitness level, create a custom workout plan, provide exercise instruction, hands-on form and technique corrections,
-                motivation and support, progress tracking, safety and injury prevention, workout variety, education, and provide feedback.
-                Sessions are 60 minutes, structure varies among individuals.
-              </div>
+              {dropdown1 && (
+                <div className={`training-description ${dropdown1 ? 'active' : ''}`}>
+                  Go over goals, and current fitness level, create a custom workout plan, provide exercise instruction, hands-on form and technique corrections,
+                  motivation and support, progress tracking, safety and injury prevention, workout variety, education, and provide feedback.
+                  Sessions are 60 minutes, structure varies among individuals.
+                </div>
+              )}
             </div>
             <div className="sessions-lines"></div>
             <div className="sessions-info">
@@ -95,29 +112,33 @@ const HomePage = () => {
                 <div className="training-type">GROUP IN-PERSON TRAINIING</div>
                 <div className="pricing">
                   <div>$120/hr</div>
-                  <div><i class="fa-solid fa-chevron-down"></i></div>
+                  <div onClick={toggleDropDown2} className="session-dropdown"><i className={`fa-solid fa-chevron-${dropdown2 ? 'down' : 'up'}`}></i></div>
                 </div>
               </div>
-              <div>
-                Go over goals, and current fitness level(s), create a custom workout plan(s), provide exercise instruction, hands-on form and technique corrections,
-                motivation and support, progress tracking, safety and injury prevention, workout variety, education, and provide feedback.
-                Sessions are 60 minutes, structure varies among individuals.
-              </div>
+              {dropdown2 && (
+                <div className={`training-description ${dropdown2 ? 'active' : ''}`}>
+                  Go over goals, and current fitness level(s), create a custom workout plan(s), provide exercise instruction, hands-on form and technique corrections,
+                  motivation and support, progress tracking, safety and injury prevention, workout variety, education, and provide feedback.
+                  Sessions are 60 minutes, structure varies among individuals.
+                </div>
+              )}
             </div>
             <div className="sessions-lines"></div>
             <div className="sessions-info">
               <div className="training-and-rates">
-                <div className="training-type">GROUP IN-PERSON TRAINIING</div>
+                <div className="training-type">NUTRITION COACHING</div>
                 <div className="pricing">
                   <div>Rates vary</div>
-                  <div><i class="fa-solid fa-chevron-down"></i></div>
+                  <div onClick={toggleDropDown3} className="session-dropdown"><i className={`fa-solid fa-chevron-${dropdown3 ? 'down' : 'up'}`}></i></div>
                 </div>
               </div>
-              <div>
-                Assess, educate, plan, and monitor eating habits. Offer support, feedback, individualized advice, strategies and solutions on
-                how to improve diet to achieve personal goals and an overall healthy lifestyle.
-                Structure varies among individuals.
-              </div>
+              {dropdown3 && (
+                <div className={`training-description ${dropdown3 ? 'active' : ''}`}>
+                  Assess, educate, plan, and monitor eating habits. Offer support, feedback, individualized advice, strategies and solutions on
+                  how to improve diet to achieve personal goals and an overall healthy lifestyle.
+                  Structure varies among individuals.
+                </div>
+              )}
             </div>
             <div className="sessions-lines"></div>
             <a href="/" id="session-button">
@@ -128,9 +149,12 @@ const HomePage = () => {
 
           </div>
         </div>
+        <div className="training-image">
+          <img style={{ width: '100%', height: '100%' }} src="/images/helen3.jpeg" />
+        </div>
       </section>
       <section id="contact-section-container">
-
+        <h1>Placeholder</h1>
       </section>
     </main>
   )
